11b4fdd854837b00acdf5cf199a0bcf82aa5c7b9
ZondaF12 <ruaridhbell73@googlemail.com>
2022-02-04 00:12:29 +0000

    Initial Commit

---
+++ contracts/Mudded.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract MUDDED is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 public mintRate = 0.05 ether;
+    uint256 public MAX_SUPPLY = 1; // Only 1 NFT available for minting
+
+    constructor() ERC721("MUDDED", "MUD") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ipfs.io/ipfs/QmNRWkbvm35Af4dkxQoQgndcFBFByJCL6fvyjVKAvmbFB7";
+    }
+
+    function safeMint(address to, string memory uri) public payable { // Payable indicates that the NFT is requried to be paid for
+        require(totalSupply() < MAX_SUPPLY, "SOLD OUT!");
+        require(msg.value >= mintRate, "Not enough Ether sent");
+
+        // Starts minting from token ID 1
+        _tokenIdCounter.increment();
+        uint256 tokenId = _tokenIdCounter.current();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function withdraw() public onlyOwner{
+        require(address(this).balance > 0, "Balance is 0");
+        payable(owner()).transfer(address(this).balance);
+    }
+}
--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+import MUD from "./artifacts/contracts/Mudded.sol/MUDDED.json";
+import { getAccountPath } from "ethers/lib/utils";
+import { Web3Provider } from "@ethersproject/providers";
+
+// contract address
+const mudAddress = "0x537430782EDA32B4C8594b0412C81c5B1dE3f821";
 
 function App() {
+  // connect to metamask wallet of user
+  async function requestAccount() {
+    // prompt user to connect one of their metamask account
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+  }
+
+  // Minting my NFT
+  async function mintNFT() {
+    if (typeof window.ethereum !== "undefined") {
+      // Wait for Wallet to be connected
+      await requestAccount(); // Gives access to wallet
+      // create a new provider
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const gasPrice = await provider.getGasPrice();
+      // sign transaction using a signer, cause we will create an update in the blockchain
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(mudAddress, MUD.abi, signer);
+      //now we can create the transaction ( writing )
+      const transaction = await contract.safeMint(
+        window.ethereum.selectedAddress,
+        "https://ipfs.io/ipfs/Qmdjr4L4FHNZjbYVEQr6FPiUZyqhyBBfYUAmQDqiWBJQvT",
+        {
+          value: ethers.utils.parseEther("0.05"),
+          gasPrice: ethers.utils.parseUnits("100", "gwei"),
+          gasLimit: 1000000,
+        }
+      ); // safe mint to this address
+      // wait for the transaction to be confirmed in the blockchain ( in production environment in takes longer )
+      await transaction.wait();
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>{window.ethereum.selectedAddress}</div>
+        <button onClick={mintNFT}> Mint</button>
       </header>
     </div>
   );
